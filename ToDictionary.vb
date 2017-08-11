1cb6756baaf95004bac9db414d229acb9b054a44
Danny Hilario Suarez <dannyhilariosuarez@gmail.com>
2017-08-11 15:55:51 -0400

    Add files via upload

---
+++ ToDictionary.vb
@@ -0,0 +1,27 @@
+﻿''''''''''''''''''''''''' 
+'                       '
+'<Danny Hilario Suarez/>'
+'                       '
+'''''''''''''''''''''''''
+
+
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+
+Public Module ToDictionary
+
+    <Extension()>
+    Public Function ToDictionary(obj As DataTable) As Dictionary(Of String, List(Of String))
+        Dim dic As New Dictionary(Of String, List(Of String))
+
+        For Each col As DataColumn In obj.Columns
+            Dim list As New List(Of String)
+            For Each ro As DataRow In obj.Rows
+                list.Add(ro(col).ToString)
+            Next
+            dic.Add(col.ColumnName, list)
+        Next
+        Return dic
+    End Function
+
+End Module
